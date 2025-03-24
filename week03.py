4a4ba4eda6d758df6d376cc77682ba0fe4c7f858
202244031 <kkjenc123@gmail.com>
2025-03-24 17:16:26 +0900

    0.5 array

--- week03.py
+++ week03.py
@@ -1,4 +1,14 @@
-l = [99, 0, -7, 0, 16]
+def move_zeros(l):
+    zero_idx = 0
+    for i in range(len(l)):
+        n = l[i]
+        if n != 0:
+            l[zero_idx] = n
+            if zero_idx != i:
+                l[i] = 0
+            zero_idx = zero_idx + 1
+        return l
 
-for i in range(len(l)):
-    print(f"{l[i]:3} {id(l[i])}")
+l = [99, 0, -7, 0, 16]
+move_zeros(l)
+print(l)
