9d89e90a74b3c33b926255fbb530329e7895402d
202244031 <kkjenc123@gmail.com>
2025-04-07 18:17:43 +0900

    v2.4 stack

--- week05.py
+++ week05.py
@@ -1,9 +1,20 @@
-s1 = []
-s1.append("Data structure") # push
-s1.append("Database")
-print(s1[-1]) # peek
-print(s1)
-print(s1.pop()) # pop
-print(s1)
-print(s1.pop())
-print(s1)
+# print(1+2))
+def is_valid_parentheses(expression : str) -> bool:     #type hint
+    stack = list()
+    for letter in expression:
+        if letter == "(":
+            stack.append(letter)
+        if letter == ")":
+            if len(stack) == 0:
+                return False
+            else:
+                stack.pop()
+    return len(stack) == 0
+
+
+
+print(is_valid_parentheses(")1+2()"))
+print(is_valid_parentheses("(1+2))"))
+print(is_valid_parentheses("(1+2)"))
+print(is_valid_parentheses("(3*2)/2"))
+print(is_valid_parentheses("((3*2/2)"))
